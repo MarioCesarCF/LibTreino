b35e2afb5d2326b65653435c33e032cb5661ab75
Mario Cesar Cunha Fialho <t_mfialho@tenda.com>
2025-08-19 21:41:42 -0300

    refactor: ajusta situacao do item

--- LibTreino/Models/ViewModels/Lista/AtualizaItemListaDto.cs
+++ LibTreino/Models/ViewModels/Lista/AtualizaItemListaDto.cs
@@ -6,7 +6,7 @@ namespace LibTreino.Models.ViewModels.Lista
     {
         public string ListaComprasId { get; set; }
         public string Id { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Quantidade { get; set; }
         public int Unidade { get; set; }
         public int Situacao { get; set; }
